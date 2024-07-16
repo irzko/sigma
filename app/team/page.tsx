7dbb700fb2826009036f83f75d509bec2be628b2
irzko <tranminhkha.1003@gmail.com>
2024-07-16 11:01:26 +0700

    Add button and link to team page

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,13 +1,17 @@
 import { createClient } from "@/utils/supabase/server";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default async function Page() {
   const supabase = createClient();
   const { data: teams, error } = await supabase.from("teams").select("*");
   return (
-    <main>
-      <ul>
+    <main className="flex justify-center">
+      <ul className="max-w-screen-md w-full">
         {teams?.map((team) => (
-          <li key={team.id}>{team.name}</li>
+          <Button as={Link} href={`team/${team.id}`} key={team.id}>
+            {team.name}
+          </Button>
         ))}
       </ul>
     </main>
